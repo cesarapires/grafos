ec0377e6a4126ba8ca82e31c285a789600511728
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-17 22:56:14 -0300

    feat: refatora funções de leitura e escrita, melhora a legibilidade e corrige formatação dos arquivos de saída

--- cpa/mscsol.cpp
+++ cpa/mscsol.cpp
@@ -5,92 +5,100 @@
 
 using namespace std;
 
-vector<int> lerMatrizInt(ifstream &in, int linhas, int colunas) {
-    vector<int> dados;
+vector<int> lerMatrizInt(ifstream &arquivoEntrada, int totalLinhas, int totalColunas) {
+    vector<int> matriz;
+    matriz.reserve(totalLinhas * totalColunas);
 
-    dados.reserve(linhas * colunas);
-    
-    for (int i = 0; i < linhas; ++i) {
-        for (int j = 0; j < colunas; ++j) {
-            int x;
-            in >> x;
-            dados.push_back(x);
+    for (int linha = 0; linha < totalLinhas; ++linha) {
+        for (int coluna = 0; coluna < totalColunas; ++coluna) {
+            int valor;
+            arquivoEntrada >> valor;
+            matriz.push_back(valor);
         }
     }
-    
-    return dados;
+    return matriz;
 }
 
-bool escreverSolucao(const string& outName, const vector<int>& sol) {
-    ofstream out(outName);
-    
-    if (!out) {
-        cerr << "Nao foi possivel criar " << outName << endl;
+bool escreverSolucao(const string& nomeArquivoSaida, const vector<int>& solucao) {
+    ofstream arquivoSaida(nomeArquivoSaida);
+
+    if (!arquivoSaida) {
+        cerr << "Não foi possível criar " << nomeArquivoSaida << endl;
         return false;
     }
 
-    for (size_t k = 0; k < sol.size(); ++k) {
-        out << sol[k];
-        if (k + 1 < sol.size()) out << ' ';
+    for (size_t index = 0; index < solucao.size(); ++index) {
+        arquivoSaida << solucao[index];
+        if (index + 1 < solucao.size()) {
+            arquivoSaida << ' ';
+        }
     }
-    
+
     return true;
 }
 
-vector<int> construirSolucao(const vector<int>& vetor, int n, int colunas, const vector<int>& dadosRastreio) {
-    vector<int> sol;
-    
-    int i = 0;
-    int j = 1;
+vector<int> construirSolucao(const vector<int>& dadosVetorOriginal,
+                             int tamanhoVetor,
+                             int totalColunas,
+                             const vector<int>& matrizRastreio) 
+{
+    vector<int> solucao;
 
-    while (j <= n) {
-        int col = j - 1;
-        int mark = dadosRastreio[i * colunas + col];
-        if (mark == 1) {
-            sol.push_back(vetor[j - 1]);
-            i = j;
-            ++j;
-        } else {
-            ++j;
+    int linhaAtual = 0;
+    int elementoAtual = 1;
+
+    while (elementoAtual <= tamanhoVetor) {
+        int colunaIndex = elementoAtual - 1;
+        int marcaRastreio = matrizRastreio[linhaAtual * totalColunas + colunaIndex];
+
+        if (marcaRastreio == 1) {
+            solucao.push_back(dadosVetorOriginal[elementoAtual - 1]);
+            linhaAtual = elementoAtual;
         }
+
+        ++elementoAtual;
     }
-    
-    return sol;
+
+    return solucao;
 }
 
 int main(int argc, char* argv[]) {
-    vector<string> saidas = {"saida1.txt", "saida2.txt", "saida3.txt"};
-    for (size_t idx = 0; idx < saidas.size(); ++idx) {
-        const string& nome = saidas[idx];
-        ifstream in(nome);
-        if (!in) {
-            cerr << "Nao foi possivel abrir " << nome << endl;
+    vector<string> arquivosEntrada = {"saida1.txt", "saida2.txt", "saida3.txt"};
+
+    for (size_t indiceArquivo = 0; indiceArquivo < arquivosEntrada.size(); ++indiceArquivo) {
+        const string& nomeArquivoEntrada = arquivosEntrada[indiceArquivo];
+        ifstream arquivoEntrada(nomeArquivoEntrada);
+
+        if (!arquivoEntrada) {
+            cerr << "Não foi possível abrir " << nomeArquivoEntrada << endl;
             continue;
         }
 
-        int n;
-        in >> n;
-        if (!in || n <= 0) {
-            cerr << "Formato invalido em " << nome << " (n)" << endl;
+        int tamanhoVetor;
+        arquivoEntrada >> tamanhoVetor;
+
+        if (!arquivoEntrada || tamanhoVetor <= 0) {
+            cerr << "Formato inválido em " << nomeArquivoEntrada << " (tamanhoVetor)" << endl;
             continue;
         }
 
-        vector<int> vetor(n);
-        for (int i = 0; i < n; ++i) in >> vetor[i];
+        vector<int> vetorOriginal(tamanhoVetor);
+        for (int i = 0; i < tamanhoVetor; ++i) {
+            arquivoEntrada >> vetorOriginal[i];
+        }
 
-        int linhas = n + 1;
-        int colunas = n + 1;
+        int totalLinhas = tamanhoVetor + 1;
+        int totalColunas = tamanhoVetor + 1;
 
-        vector<int> dadosCusto = lerMatrizInt(in, linhas, colunas);
+        vector<int> matrizCusto = lerMatrizInt(arquivoEntrada, totalLinhas, totalColunas);
+        vector<int> matrizRastreio = lerMatrizInt(arquivoEntrada, totalLinhas, totalColunas);
 
-        vector<int> dadosRastreio = lerMatrizInt(in, linhas, colunas);
+        vector<int> solucao = construirSolucao(vetorOriginal, tamanhoVetor, totalColunas, matrizRastreio);
 
-        vector<int> sol = construirSolucao(vetor, n, colunas, dadosRastreio);
+        string nomeArquivoSaida = "sol" + to_string(indiceArquivo + 1) + ".txt";
 
-        string outName = "sol" + to_string(idx + 1) + ".txt";
-        
-        if (!escreverSolucao(outName, sol)) continue;
+        escreverSolucao(nomeArquivoSaida, solucao);
     }
-    
+
     return 0;
 }
